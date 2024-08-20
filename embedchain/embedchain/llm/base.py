da7b96aad50e5b2101f9245086b83146d288784b
Christos Aniftos <aniftos@google.com>
2024-08-20 13:26:24 +0100

    formatting applied

--- embedchain/embedchain/llm/base.py
+++ embedchain/embedchain/llm/base.py
@@ -5,14 +5,11 @@ from typing import Any, Optional
 
 from langchain.schema import BaseMessage as LCBaseMessage
 
-from embedchain.constants import SQLITE_PATH
 from embedchain.config import BaseLlmConfig
 from embedchain.config.llm.base import (
-    DEFAULT_PROMPT,
-    DEFAULT_PROMPT_WITH_HISTORY_TEMPLATE,
-    DEFAULT_PROMPT_WITH_MEM0_MEMORY_TEMPLATE,
-    DOCS_SITE_PROMPT_TEMPLATE,
-)
+    DEFAULT_PROMPT, DEFAULT_PROMPT_WITH_HISTORY_TEMPLATE,
+    DEFAULT_PROMPT_WITH_MEM0_MEMORY_TEMPLATE, DOCS_SITE_PROMPT_TEMPLATE)
+from embedchain.constants import SQLITE_PATH
 from embedchain.core.db.database import init_db, setup_engine
 from embedchain.helpers.json_serializable import JSONSerializable
 from embedchain.memory.base import ChatHistory
--- embedchain/tests/llm/conftest.py
+++ embedchain/tests/llm/conftest.py
@@ -1,6 +1,7 @@
-
 from unittest import mock
+
 import pytest
+
 
 @pytest.fixture(autouse=True)
 def mock_alembic_command_upgrade():
